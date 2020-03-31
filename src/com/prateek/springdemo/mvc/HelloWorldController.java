1ea063d0310e4e0fcc89f4932f125cebf1dac5ce
Prateek <prateek.verma0701@gmail.com>
2020-04-01 03:40:53 +0530

    Removed ambiguity when multiple controller have same mapping.

--- src/com/prateek/springdemo/mvc/HelloWorldController.java
+++ src/com/prateek/springdemo/mvc/HelloWorldController.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	// need a controller method to show the initial form
