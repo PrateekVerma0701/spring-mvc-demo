6c98df369a7ad9d13f2f8213bed04299e8700db1
Prateek <prateek.verma0701@gmail.com>
2020-04-02 03:45:17 +0530

    process to add your own custom message over spring generated message

--- src/com/prateek/springdemo/mvc/CustomerController.java
+++ src/com/prateek/springdemo/mvc/CustomerController.java
@@ -34,6 +34,35 @@ public class CustomerController {
 	@RequestMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
 
+		// Debugging trick to apply your own custom messages by overriding values
+		
+		// Step-1 - Check binding result messages
+		
+		// Step-2 - Override message value with your own custom message
+
+		/*
+		 * Binding Result : org.springframework.validation.BeanPropertyBindingResult: 1
+		 * errors Field error in object 'customer' on field 'freePasses': rejected value
+		 * [g]; codes
+		 * [typeMismatch.customer.freePasses,typeMismatch.freePasses,typeMismatch.java.
+		 * lang.Integer,typeMismatch]; arguments
+		 * [org.springframework.context.support.DefaultMessageSourceResolvable: codes
+		 * [customer.freePasses,freePasses]; arguments []; default message
+		 * [freePasses]]; default message [Failed to convert property value of type
+		 * 'java.lang.String' to required type 'java.lang.Integer' for property
+		 * 'freePasses'; nested exception is java.lang.NumberFormatException: For input
+		 * string: "g"]
+		 */
+
+		/*
+		 * Step-3- In message.properties define the value of
+		 * typeMismatch.customer.freePasses
+		 * 
+		 * typeMismatch.customer.freePasses= Invalid Number
+		 */
+
+		System.out.println("Binding Result : " + bindingResult);
+
 		if (bindingResult.hasErrors()) {
 			return "customer-form";
 		} else {
