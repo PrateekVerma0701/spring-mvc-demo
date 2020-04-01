ffa7bee14e7488a31aee4bfc8c95af96127d437d
Prateek <prateek.verma0701@gmail.com>
2020-04-02 00:45:25 +0530

    Resolving whitespace issue during validation using InitBinder Annotation

--- src/com/prateek/springdemo/mvc/CustomerController.java
+++ src/com/prateek/springdemo/mvc/CustomerController.java
@@ -2,15 +2,29 @@ package com.prateek.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+	// add a InitBinder to convert trim input strings
+	// remove leading and trailing whitespace
+	// resolve issue for our validation
+
+	@InitBinder
+	public void initBinder(WebDataBinder databinder) {
+
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		databinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
@@ -20,6 +34,9 @@ public class CustomerController {
 
 	@RequestMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+
+		// checking white space issue
+		System.out.println("Last Name : " + "|" + customer.getLastName() + "|");
 
 		if (bindingResult.hasErrors()) {
 			return "customer-form";
